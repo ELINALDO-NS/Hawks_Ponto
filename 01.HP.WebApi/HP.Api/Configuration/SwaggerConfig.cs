e741a13577a2051c356ad6891b89815be82d32f4
Elinaldo Nascimento <elinaldo_nascimento@outlook.com>
2026-07-13 13:39:22 -0300

    Chore: Configurações iniciais

---
+++ 01.HP.WebApi/HP.Api/Configuration/SwaggerConfig.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.OpenApi;
+
+namespace HP.Api.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Hawks Ponto",
+                    Version = "v1",
+                    Description = "API da aplicação Hawks Ponto",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Elinaldo Nascimeto",
+                        Email = "elinaldo_nascimento@Outlook.com"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "OSD",
+                        Url = new System.Uri("https://opensource.org/osd")
+                    },
+                    TermsOfService = new System.Uri("https://opensource.org/osd")
+
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta forma: Bearer {seu_token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT"
+                });
+
+                options.AddSecurityRequirement(document =>
+                {
+                    var schemeRef = new OpenApiSecuritySchemeReference("Bearer", document);
+
+                    return new OpenApiSecurityRequirement
+                    {
+                        [schemeRef] = Array.Empty<string>().ToList()
+                    };
+                });
+
+
+            });
+        }
+        public static void UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+        }
+    }
+}
---
+++ 01.HP.WebApi/HP.Api/Program.cs
@@ -0,0 +1,48 @@
+using HP.Api.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddSwaggerConfiguration();
+// Add services to the container.
+
+var app = builder.Build();
+app.UseCors();
+app.UseSwaggerConfiguration();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
